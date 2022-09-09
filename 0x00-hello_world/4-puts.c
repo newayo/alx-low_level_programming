b5e19fecf020347ee7123120126585fa13ad2b17
newayo <newayo@yahoo.ca>
2022-09-08 21:51:39 -0700

    all of c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <stdio.h>
+ /**
+  * main - entry point
+  * Return is always 0
+int main (void)
+{
+  puts("\"Programming is like building a multilingual puzzle")
+	return(o);
+	}
