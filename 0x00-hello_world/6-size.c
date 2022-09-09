68e24e85cf1f8486ffd1b056c1d9b7b3739b766f
newayo <newayo@yahoo.ca>
2022-09-09 01:21:00 -0700

    this 6 hmmm

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,14 +2,13 @@
 # include <stdio.h>
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
  *
- *   *
  *
- *    * Return: always 0
+ * Return: always 0
  *
- *    */
+ */
 
 int main(void)
 {
