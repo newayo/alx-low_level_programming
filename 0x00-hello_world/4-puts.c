9a9faff8fd0025331db3fad60138881d812fd861
newayo <newayo@yahoo.ca>
2022-09-09 00:55:38 -0700

    4 na ni

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,11 @@
  * main - print the string in the put function
  *
  * description: using the main function
- * this program prints "programming is like building a multiingual puzzle'
+ * this program prints "programming is like building a multiingual puzzle
  * Return: 0
  */
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
-}
-		
+}	
