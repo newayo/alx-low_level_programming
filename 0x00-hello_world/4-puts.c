52b36ee4f1fff2c45cfcaabcd55eedb7760d0504
newayo <newayo@yahoo.ca>
2022-09-08 22:51:07 -0700

    na 4 b dis?

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@
   * main - print the string in the put function
   * description: using main function
   * this program puts "programming is like building a multilingual puzzle" 
-  * Return is always 0
+  * Return: Always 0
   */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+return(0);
 }
