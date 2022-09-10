621b8d10cc96d123b98911f47832e5d13928cf04
newayo <newayo@yahoo.ca>
2022-09-10 11:42:55 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+/**
+ * main - print if the number is positive, zero or negative
+ *
+ * Description: using the main function, this program prints"programming is positive, zero and negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57 ; i++)
+	{
+		putchar (i);
+		if (i != 57)
+		{
+			putchar (',');
+			putchar (' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}	
