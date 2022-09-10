d8a7ba197a2e7100c9b42a62832adbd7c8a96c11
newayo <newayo@yahoo.ca>
2022-09-10 06:51:28 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- the program print if the number is positive, zero or negative
+ *
+ * Description: the program prints using the main
+ * function "Programming is positive, zero or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z' ; ch >= 'a' ; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
