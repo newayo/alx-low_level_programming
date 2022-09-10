c65369d1cb41b8e3294cb9c942ed3eb622e5fb8b
newayo <newayo@yahoo.ca>
2022-09-10 04:16:14 -0700

    2 pt alp

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,15 @@
 /**
  * main - prints if the number is positivr, zero or negative
  *
- * Description: using the main function, this program prints, "Programming is positive, zero or negative
+ * Description: using the main function, this program
+ * prints, "Programming is positive, zero or negative
  * Return: 0
  */
 
 int main(void)
 {
 	char ch;
+	
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
 	putchar(ch);
