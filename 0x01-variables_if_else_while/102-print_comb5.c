0299954078cb9d59eff9f33ae1c895cac132e319
newayo <newayo@yahoo.ca>
2022-09-12 04:19:50 -0700

    just do

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,11 +17,11 @@ int main(void)
 		{
 			if (j != i)
 			{
-				putchar((i / 10) + '48');
-				putchar((i % 10) + '48');
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
-				putchar((j / 10) + '48');
-				putchar((j % 10) + '48');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
 				if (i != 98 || j != 98)
 				{
 					putchar(',');
