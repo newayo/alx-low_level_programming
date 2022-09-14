69b9a4293b15700f3a1dbc339765dc1d77ac103a
newayo <newayo@yahoo.ca>
2022-09-14 13:57:04 -0700

    2 alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include 'main.h'
+/**
+ * print - alphabeth function to print abc
+ *
+ * Return: 0
+ */
+void print_alphabeth(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
