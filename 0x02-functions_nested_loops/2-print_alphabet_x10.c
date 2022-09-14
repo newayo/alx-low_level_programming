be43dab17cae550b36267a41b69533819dec3417
newayo <newayo@yahoo.ca>
2022-09-14 14:41:42 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function prints abc ten times
+ *
+ * Return: 0
+ */
+int print_alphabet_x10(void)
+{
+	char c, i;
+
+	for(i = 0; i<=0; i++)
+	{
+		for(c = 'a'; c <= 'z'; c++)
+		{
+		 	_putchar(c);
+		}
+			_putchar(i);
+	}
+_putchar('\n');	
