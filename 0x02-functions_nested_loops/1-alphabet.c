c066d12e1223e89a2c9874e8de6e58575951625e
newayo <newayo@yahoo.ca>
2022-09-14 14:24:20 -0700

    main alfa

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print - alphabeth function to print abc
+ * print - alphabet function to print abc
  *
  * Return: 0
  */
-void print_alphabeth(void)
+void print_alphabet(void)
 {
 	char c;
 	for (c = 'a'; c <= 'z'; c++)
