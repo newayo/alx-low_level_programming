e88c4bdb0f36637d3cd0751dc76260933e6e9ee3
newayo <newayo@yahoo.ca>
2022-09-15 04:02:29 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints out the first 50
+ * fibonacci suit numbers
+ * Return: return 0
+ */
+int main(void)
+{
+	int inc;
+	unsigned long n1 = 0, n2 = 1, n3;
+
+	for (inc = 0; inc < 98; inc++)
+	{
+		n3 = n1 + n2;
+		printf("%lu", n3);
+		n1 = n2;
+		n2 = n3;
+		if (inc = = 97)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
