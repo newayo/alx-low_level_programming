6ffc8e28153ac7ece1615dccc211c8234efdbb75
newayo <newayo@yahoo.ca>
2022-09-14 22:05:07 -0700

    100t

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,15 +22,15 @@ void print_times_table(int n)
 				if (result <= 99)
 					_putchar(' ');
 				if (result <= 9)
-				       _putchar(' ');
+			 	        _putchar(' ');
 			if (result >= 100)
 			{
 			_putchar((result / 100) + '0');
-			_putchar((result / 100) % 10 + '0');
+			_putchar((result / 10) % 10 + '0');
 			}
 			else if (result <= 99 && result >= 10)
 			{
-				_putchar((result /10) + '0');
+				_putchar((result / 10) + '0');
 			}
 			_putchar((result % 10) + '0');
 			}
