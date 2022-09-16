9a85a0102183739d84b3be87fddd93eba411ba98
newayo <newayo@yahoo.ca>
2022-09-16 12:33:04 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,7 +9,7 @@
 
 void print_numbers(void);
 void print_most_numbers(void);
-void print_more_numbers(void);
+void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
@@ -17,7 +17,7 @@ void print_triangle(int size);
 void print_number(int n);
 int _putchar(char);
 int _isupper(int);
-int _indigit(int);
+int _isdigit(int);
 int mul(int int);
 
 #endif /*MAIN_H */
