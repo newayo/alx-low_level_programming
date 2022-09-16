4fa8ce93a18f53516fe8706df089f0bdbdaa9db1
newayo <newayo@yahoo.ca>
2022-09-16 12:20:48 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
  * _isupper - checks if a character is
  * uppercase or not
  * @c: character to be tested
- * Return: 1 whether it is 0 otherwise
+ * Return: 1 whether it is, 0 otherwise
  */
 int _isupper(int c)
 {
