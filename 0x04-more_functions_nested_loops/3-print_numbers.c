e1d4a96f32e0e82d22e17e8de812be87e5268ad0
newayo <newayo@yahoo.ca>
2022-09-16 22:54:11 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_numbers - function that print all numbers from
- * 0 to 9
+ * print_numbers - function that print all
+ * numbers from 0 to 9
  *
  * Return: returns nothing
  */
