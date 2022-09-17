f4f4c92d5243eac2d4758d1797dcf411d12ccf1b
newayo <newayo@yahoo.ca>
2022-09-16 22:33:17 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,10 +13,13 @@ void print_square(int size)
 	{
 		for (inc1 = 0; inc1 < size; inc1++)
 		{
+			for (inc2 = 0; inc2 < (size - 1); inc2++)
+			{
+				putchar('#');
+			}
 			putchar('#');
+			putchar('\n');
 		}
-		putchar('#');
-		putchar('\n');
 	}
 	else
 	{
