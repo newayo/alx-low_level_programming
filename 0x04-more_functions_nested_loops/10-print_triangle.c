4ab5f1ad5c0cd621a8d24dee195281c3de221de2
newayo <newayo@yahoo.ca>
2022-09-16 21:59:48 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *
- * *print_triangle - prints a triangle
- * *@size:size parameter of triangle
- * *Return: returns nothing
+ * print_triangle - prints a triangle
+ * @size:size parameter of triangle
+ * Return: returns nothing
  */
 void print_triangle(int size)
 {
