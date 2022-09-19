600bd862a36ea578fb38cb674da18d7fd221bb94
newayo <newayo@yahoo.ca>
2022-09-19 08:10:44 -0700

    1 n 2

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+/**
+ * void prototype()
+ * int prototype()
+ */
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int *b);
+char *_stropy(char *dest, char *src);
+int _atoi(char *str);
+
+#endif /* MAIN.H */
