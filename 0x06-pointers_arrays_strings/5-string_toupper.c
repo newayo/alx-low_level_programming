d9408be148cb6707639b34dd57e5f9598fb1eb60
newayo <newayo@yahoo.ca>
2022-09-22 08:30:11 -0700

    na 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,7 +12,7 @@ char *string_toupper(char *str)
 
 	while (str[i] != '\0')
 	{
-		if ((str[i] >= 97) && (str[i] <= 122))
+		if ((str[i] >= 'a') && (str[i] <= 'z'))
 			str[i] = str[i] - 32;
 		i++;
 	}
