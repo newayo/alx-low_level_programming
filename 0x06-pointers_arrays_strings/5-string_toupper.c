c1d9ae004e24c6a258f45ba8f3822512061ab95d
newayo <newayo@yahoo.ca>
2022-09-22 06:46:51 -0700

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,19 +3,19 @@
  * string_toupper - Capitalize whole string.
  * @s: string to be capitalized
  * 
- * Return: Always 0.
+ * Return: Always return address to the string.
  */
-char *string_toupper(char *s)
+char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (str[i] != '\0')
 	{
-		if ((s[i] >= 97) && (s[i] <= 122))
+		if ((str[i] >= 97) && (str[i] <= 122))
 		{
-			s[i] = s[i] - 32;
+			str[i] = str[i] - 32;
 		}
 		i++;
 	}
-	return (s);
+	return (str);
 }
