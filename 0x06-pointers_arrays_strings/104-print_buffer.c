6a89ed4a6dc8fad9988f46de73efa5794574c128
newayo <newayo@yahoo.ca>
2022-09-22 21:18:49 -0700

    na 104

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -29,7 +29,8 @@ void print_line(char *c, int s, int l)
 			putchar('.');
 	}
 }
-/**print_buffer - prints a buffer
+/*
+ * print_buffer - prints a buffer
  * @b: buffer to print
  * @size: size of buffer
  * Return: void
