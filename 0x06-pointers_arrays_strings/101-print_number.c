9ca8d023bf041a8f84ae29ff8cb42641c3f44b49
newayo <newayo@yahoo.ca>
2022-09-24 06:16:40 -0700

    na 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_number - print an integer
+ * print_number - prints an integer
  * @n: integer to be printed
  *
  */
@@ -10,7 +10,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('-')a;
+		_putchar('-');
 		num = -num;
 	}
 	if ((num / 10) > 0)
