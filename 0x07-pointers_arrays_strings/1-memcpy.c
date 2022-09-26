75e9f905ee74f289892f8b0db69602d8725bcbdf
newayo <newayo@yahoo.ca>
2022-09-26 07:38:58 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#define "main.h"
+/**
+ * _memcpy: copies a memory area
+ * @dest:  memory area to be copied to 
+ * @src: memory area to be copied from
+ * @n: number of bytes to be copied
+ *
+ * Return - Pointer to the memory block
+ */
+char _memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
