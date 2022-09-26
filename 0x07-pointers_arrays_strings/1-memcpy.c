6a89787a48d35691fe3dd6a48eedcaa09fc6e759
newayo <newayo@yahoo.ca>
2022-09-26 13:47:41 -0700

    1n2

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,7 +7,7 @@
 *
 * Return: Pointer to the copied memory block
 */
-char*_memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
