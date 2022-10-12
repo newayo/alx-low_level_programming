06e1a0c45c69d199b0679a6826c098a1b6f47baa
newayo <newayo@yahoo.ca>
2022-10-12 15:49:33 -0700

    the tasks

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,5 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-#include <stdio.h>
-#include <stdlib>
 typedef __SIZE_TYPE__ size_t;
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
